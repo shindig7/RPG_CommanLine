5eff3c40a63c022af59d64ef785defa349f378bb
shindig7 <jal457@drexel.edu>
2017-05-03 15:24:23 -0400

    Removed Accessors for People class

--- Main.py
+++ Main.py
@@ -3,40 +3,21 @@ import re
 import csv
 
 def Person(object):
-	def __init___(self, name, str, dex, stl, per, hp, chp)
+	def __init___(self, name, strength, dex, stl, per, hp, chp)
 		self.Name = name
-		self.Strength = str
+		self.Strength = strength
 		self.Dexterity = dex
 		self.Stealth = stl
 		self.Perception = per
 		self.Health = hp
 		self.Current_HP = chp
 	
-	# Accessors
-	def getName(self):
-		return self.Name
+	def takeDmg(self, dmg):
+		self.Current_HP -= dmg
+
+	def heal(self, hp):
+		self.Current_HP += hp
 	
-	def getStrength(self):
-		return self.Strength
+def Enemy(object):
+
 	
-	def getDexterity(self):
-		return self.Dexterity
-	
-	def getStealth(self):
-		return self.Stealth
-	
-	def getPerception(self):
-		return self.Perception
-	
-	def getHealth(self):
-		return self.Health
-	
-	def getCurrentHP(self):
-		return self.Current_HP
-	
-	#  Mutators
-	def takeDamage(self, damage):
-		self.Current_HP -= damage
-	
-	def setCurrentHP(self, hp)
-		self.Current_HP = hp
